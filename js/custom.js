ef10829c27e1787da7687bc4a036b6d88cd4c50a
abdulku1234 <abdulkuddus@gmail.com>
2023-06-21 01:02:43 -0700

    8fue

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+let x =['Abdul'];
+let y =[...x,'Kuddus']
+
+console.log(y);
+
+let n1=['Twitter']
+
+let news= [...n1,'boss Tamil Nadu minister V Senthil Balaji has undergone a coronary artery bypass at Chennai underwent beating heart coronary artery bypass surgery... four bypass grafts were placed and coronary revascularisation was established currently hemodynamically stable and (is) being monitored in the post-operative cardiothoracic intensive care unit' ]
+
+console.log(news);
